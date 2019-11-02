cc0ac55ec4286532a0f457b445cb065e3bba9a6a
ubuntu <cskhy16@postech.ac.kr>
2019-11-02 03:56:41 -0700

    check address

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,15 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "userprog/pagedir.h"
+#include "threads/init.h"
+#include "devices/shutdown.h"
+#include "filesys/file.h"
+#include "filesys/filesys.h"
+#include "userprog/process.h"
+#include "devices/input.h"
+#include "threads/malloc.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -19,34 +28,61 @@ syscall_handler (struct intr_frame *f UNUSED)
 
   int * p = f->esp;
 
+  address_checking(*p);
+
   switch(*p){
-  	case SYS_HALT:
-  		break;
-  	case SYS_EXIT:
-  		break;
-  	case SYS_EXEC:
-  		break;
-  	case SYS_WAIT:
-  		break;
-  	case SYS_CREATE:
-  		break;
-  	case SYS_REMOVE:
-  		break;
-  	case SYS_OPEN:
-  		break;
-  	case SYS_FILESIZE:
-  		break;
-  	case SYS_READ:
-  		break;
-  	case SYS_WRITE:
-  		break;
-  	case SYS_SEEK:
-  		break;
-  	case SYS_TELL:
-  		break;
-  	case SYS_CLOSE:
-  		break;
+    case SYS_HALT:
+      halt();
+      break;
+    case SYS_EXIT:
+      break;
+    case SYS_EXEC:
+      break;
+    case SYS_WAIT:
+      break;
+    case SYS_CREATE:
+      break;
+    case SYS_REMOVE:
+      break;
+    case SYS_OPEN:
+      break;
+    case SYS_FILESIZE:
+      break;
+    case SYS_READ:
+      break;
+    case SYS_WRITE:
+      break;
+    case SYS_SEEK:
+      break;
+    case SYS_TELL:
+      break;
+    case SYS_CLOSE:
+      break;
   }
 
-  thread_exit ();
+}
+
+void address_checking(int * p){
+  if(!is_user_vaddr(p)){
+    printf("not user vaddr");
+    exit(-1);
+  }
+}
+
+void halt(void){
+  shutdown_power_off();
+}
+
+void exit(int status){
+  thread_current()->exit_value = status;
+  printf("%s: exit(%d)\n", thread_current()->name, status);
+  thread_exit();
+}
+
+pid_t exec(const char* cmd_line){
+  return process_execute(cmd_line);
+}
+
+int wait(pid_t pid){
+  return process_wait(pid);
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,14 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+typedef int pid_t;
+
 void syscall_init (void);
 
+void address_checking(int * p);
+
+void halt(void);
+void exit(int status);
+pid_t exec(const char* cmd_line);
+int wait(pid_t pid);
 #endif /* userprog/syscall.h */
