61745de97eaefa31e796f3a73c39cef189565857
ubuntu <hhk971@postech.ac.kr>
2019-11-20 07:21:26 -0800

    stable alloc less function

--- src/vm/page.c
+++ src/vm/page.c
@@ -1,1 +1,20 @@
 #include "vm/page.h"
+
+
+void* stable_alloc(void* addr, struct file* file, size_t offset, size_t read_bytes, mapid_t mapid){
+    struct stable_entry *entry = malloc(sizeof(struct stable_entry));
+    entry->vaddr = addr;
+    entry->file = file;
+    entry->offset = offset;
+    entry->read_bytes = read_bytes;
+    entry->is_loaded = false;
+    entry->zero_bytes = PGSIZE - read_bytes;
+    entry->mapid = mapid;
+    return addr + PGSIZE;
+}
+
+bool stable_less(struct hash_elem *a, struct hash_elem *b){
+    struct stable_entry * entry_a = hash_entry(a, struct stable_entry, elem); 
+    struct stable_entry * entry_b = hash_entry(b, struct stable_entry, elem);   
+    return entry_a->vaddr < entry_b->vaddr; 
+}
--- src/vm/page.h
+++ src/vm/page.h
@@ -1,20 +1,25 @@
 #ifndef VM_PAGE_H
 #define VM_PAGE_H
 #include <hash.h>
+#include "lib/user/syscall.h"
+#include <syscall-nr.h>
+#include "threads/vaddr.h"
 #include "filesys/file.h"
 
 struct stable_entry {
     void* vaddr; // virtual page address
     size_t offset; // page offset
-    bool isLoaded; // Is loaded on physical memory
+    bool is_loaded; // Is loaded on physical memory
     struct file * file; // File pointer for read file
-    size_t readBytes; // To read bytes
-    size_t zeroBytes; // size of extra 0 
+    size_t read_bytes; // To read bytes
+    size_t zero_bytes; // size of extra 0 
     struct hash_elem elem; // hash table element
     mapid_t mapid; // mmap id
     size_t swap_index; // swap index
-}
+};
 
+void* stable_alloc(void* addr, struct file* file, size_t offset, size_t read_bytes, mapid_t mapid);
+bool stable_less(struct hash_elem *a, struct hash_elem *b);
 
 
 #endif
