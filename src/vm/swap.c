173d8551a2a51a9ea11ec9d1f1a674ad1eb9fc49
ubuntu <cskhy16@postech.ac.kr>
2019-11-23 05:45:04 -0800

    swap file change

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -1,1 +1,49 @@
 //swap.c
+#include "vm/swap.h"
+
+struct block *swap_block;
+struct bitmap *swap_map;
+
+#define SECTORS_PER_PAGE (PGSIZE / BLOCK_SECTOR_SIZE)
+
+void swap_init(void){
+	swap_block = block_get_role(BLOCK_SWAP);
+	if(swap_block){
+		swap_map = bitmap_create(block_size(swap_block) / SECTORS_PER_PAGE);
+		if(swap_map){
+			bitmap_set_all(swap_map, 0);
+		}
+		else{
+			return;
+		}
+	}
+	else{
+		return;
+	}
+}
+
+void swap_in(size_t swap_index, void * frame_page){
+	if(swap_map && swap_block){
+		for(size_t i = 0; i < SECTORS_PER_PAGE; i++){
+		block_read(swap_block, swap_index * SECTORS_PER_PAGE + i, (uint8_t *) frame + i * BLOCK_SECTOR_SIZE);
+		return;
+	}
+	else{
+		return;
+	}
+}
+
+size_t swap_out(void * frame_page){
+	if(swap_map && swap_block){
+		size_t free_index = bitmap_scan_and_flip(swap_map, 0, 1, 0)
+
+		for(size_t i = 0; i < SECTORS_PER_PAGE; i++){
+			block_write(swap_block, free_index * SECTORS_PER_PAGE + i, (uint8_t *) frame + i * BLOCK_SECTOR_SIZE);
+		}
+
+		return free_index;
+	}
+	else{
+		return -1;
+	}
+}
--- src/vm/swap.h
+++ src/vm/swap.h
@@ -1,1 +1,8 @@
 //swap.h
+#include <bitmap.h>
+#include "devices/block.h"
+#include "threads/vaddr.h"
+
+void swap_init(void);
+void swap_in(size_t swap_index, void * frame_page);
+size_t swap_out(void * frame_page);
