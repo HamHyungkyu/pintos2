6e07f6a32ce295c8b2ac67ec2723ebd661677778
ubuntu <hhk971@postech.ac.kr>
2019-11-28 06:59:48 -0800

    pt-write-code2 pass

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -165,10 +165,11 @@ page_fault (struct intr_frame *f)
    //   printf("user %d write %d addr %x\n", user, write, fault_addr);
      exit(-1);
   }
-  if(!not_present && user && write){
+  if(!not_present  && write){
      // try to write existing code data
      exit(-1);
   }
+
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
